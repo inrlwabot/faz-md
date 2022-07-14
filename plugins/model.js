0c11d6a7df6206b28c4d3d1106cc8858354a844f
Fasweeh-mavoor <105922035+inrlwabot@users.noreply.github.com>
2022-07-14 08:24:08 +0530

    INrlTeam

---
+++ plugins/model.js
@@ -0,0 +1,12 @@
+/* Copyright (C) 2020 INrlTeam.
+
+Licensed under the  GPL-3.0 License;
+
+you may not use this file except in compliance with the License.
+
+inrl-md
+
+*/
+//its created for undarstnging easly
+const { bot } = require('./inrl')
+const { imageToWebp, videoToWebp, writeExifImg, writeExifVid, writeExif,formatp, tanggal, formatDate, getTime, clockString, runtime, jsonformat, format, parseMention, getRandom, smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('./lib/plugin')
